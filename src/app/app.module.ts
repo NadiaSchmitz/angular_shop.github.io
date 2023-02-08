3cf3f18507d8d0b5930d34c89021d35077de4148
NadiaSchmitz <nadiiaschmitz@gmail.com>
2023-02-08 14:48:45 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './content/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
